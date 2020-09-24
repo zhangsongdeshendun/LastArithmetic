2f7cfc5241d6e00b78af5435b1ddc4551d64d32e
zhangsong <zhangs@inke.cn>
2020-09-24 16:02:34 +0800

    IsBalancedTest

---
+++ src/leetcodeTest/IsBalancedTest.java
@@ -0,0 +1,39 @@
+package leetcodeTest;
+
+public class IsBalancedTest {
+    private boolean isBalanced = true;
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        check(root);
+        return isBalanced;
+
+
+    }
+
+    public int check(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = check(root.left) + 1;
+        int right = check(root.right) + 1;
+        if (Math.abs(left - right) > 1) {
+            isBalanced = false;
+        }
+        return Math.max(left, right);
+
+    }
+}
